378745870e486a81c77dd541cac0a8181944fb4d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-15 21:24:46 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
